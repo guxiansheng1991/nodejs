136a5f6fa2ca45a6f784cc49d9742055cc578cd5
shenyuchao_1 <897100198@qq.com>
2019-07-18 13:01:31 +0800

    图表

--- course/base/blog-1/src/config/db.js
+++ course/base/blog-1/src/config/db.js
@@ -1,7 +1,9 @@
 const env  = process.env.NODE_ENV;
 let MYSQL_CONFIG = {};
+let REDIS_CONF = {};
 
 if (env === 'dev') {
+  // mysql 配置
   MYSQL_CONFIG = {
     host: 'localhost',
     user: 'root',
@@ -9,15 +11,28 @@ if (env === 'dev') {
     port: '3306',
     database: 'myblog'
   };
+
+  // redis配置
+  REDIS_CONF = {
+    port: 6327,
+    host: '127.0.0.1'
+  };
 }
 
 if (env === 'production') {
+  // mysql 配置
   MYSQL_CONFIG = {
     host: 'localhost',
     user: 'root',
     password: '123456',
     port: '3306',
     database: 'myblog'
+  };
+
+  // redis配置
+  REDIS_CONF = {
+    port: 6327,
+    host: '127.0.0.1'
   };
 }
 
