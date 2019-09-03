d2f863286d5ad1b547c2fa6e26f593911aff3a7a
shenyuchao_1 <897100198@qq.com>
2019-09-03 16:45:37 +0800

    redis等

--- course/frame/blog-express/app.js
+++ course/frame/blog-express/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisClient = require('./db/redis');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -12,14 +15,26 @@ var userRouter = require('./routes/user');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+const sessionStore = new RedisStore({
+  client: redisClient
+});
+app.use(session({
+  secret: 'syc_1234567890#!BBB',
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- course/frame/blog-express/routes/user.js
+++ course/frame/blog-express/routes/user.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { login } = require('../controller/user');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 router.post('/login', function (req, res, next) {
+    const { username, password } = req.body;
+    const result = login(username, password);
+    result.then(user => {
+        if (user.username) {
+            req.session.username = user.username;
+            req.session.realname = user.realname;
+            res.json(new SuccessModel());
+        } else {
+            res.json(new ErrorModel('登陆失败'));
+        }
+    });
+});
+
+router.get('/login-test', function (req, res, next) {
+    const session = req.session;
     res.json({
         errno: 0,
-        data: 'success'
+        msg: session.username
+    });
+});
+
+router.get('/session-test', function (req, res, next) {
+    const session = req.session;
+    if (session.viewNumber == null) {
+        session.viewNumber = 0;
+    }
+    session.viewNumber++;
+    res.json({
+        errno: 0,
+        viewNumber: session.viewNumber
     });
 });
 
